5109a9822facb64502310fe3855f08a5d1a074ad
Mazur <gabrielvitormzr@gmail.com>
2025-08-19 19:03:04 -0300

    estudo realizadop ate a aula N6

---
+++ estudo.js
@@ -0,0 +1,48 @@
+//usado pra comunicaçao e mandar mensagem dentro do java script
+console.log('ola mundo') 
+
+//var let e const sao usados para declaraçoes de valores
+let namorada = ("Kerem"); //pode ser mudado porem e usado e vale apenas para um bloco de codigo
+const fato = ('Amor da minha vida'); //nao pode ser mudado e vale assim como let apenas para um bloco de codigo 
+var sonho = ('vamos casar um dia'); //pode ser mudado e vale para todo o codigo em sim(cuidado ao usar para nao gerar erro de valores)
+
+//funçao sao composta pelo termo function seguido por parenteses e chaves onde a funçao executa tudo dentro do codigo
+function teste (){
+rl.question ('Vamos sair amanha as 8 horas da noite?')
+}
+
+//operadores do java script sao +(adiçao),-(subtraçao),*(multiplicaçao),/(divisao)
+console.log (5+5)
+console.log (5-5)
+console.log (5*5)
+console.log (5/5)
+
+//concatenar,e o ato de somar variveis que sao strings(texto) em uma so coisa
+console.log (' O nome dela é ' + namorada + ' ela é o ' + fato + ' e nós ' + sonho )
+
+//existe uma segunda maneira de usar variaveis como por exemplo
+ ${amor};
+
+//arrays sao vetores que armazenam diversos valores,podendo ser manipulando livrementes
+var array = [kerem,mae,julia]
+
+
+ //maneira de manipular um array
+//pop=ele exclui o ultimo elemento do vetor
+
+//push=adiciona um elemento no final do vetor
+
+//splice=remove um elemento ou mais apartir de um determinado índice,vai passar dois valores sendo o 1 valor o indice inicial e o segundo quantos elementos vao ser deletados apartir do indice 
+
+//slice=vai selecionar uma parte do vetor sendo que vai pular dois números o 1 numero sendo o índice inicial e o 2 sendo o índice limite
+
+//shift=exclui o 1 elemento do vetor
+
+//unshift=ele adiciona 1 ou mais elementos no inicio do vetor
+
+//includes=server para verificar se existe algo dentro do vetor(tem que estar dentro de um console.log)
+
+//indexOf-=vai mostrar o índice daquele elemto dentro do vetor(tem que estar dentro de um console.log)
+
+//vetor=variável que comprota diversos valores
+//ex;let rpg = ['avalom','kaya','ahrin','tirian']
